4f8a3c19d735e08ec595288e26024e14305841f4
Truong Nguyen <tmnguyen12@gmail.com>
2018-04-26 14:35:34 -0700

    update s3 information

--- enchant/attributes/default.rb
+++ enchant/attributes/default.rb
@@ -14,7 +14,12 @@ default[:enchant][:environment] = {
   "SECRET_KEY_BASE": node[:enchant][:secret_key_base],
   "DATABASE_URL": node[:enchant][:database_url],
   "RAILS_ENV": node[:enchant][:rails_env],
-  "RAILS_LOG_TO_STDOUT": node[:enchant][:log_to_stdout]
+  "RAILS_LOG_TO_STDOUT": node[:enchant][:log_to_stdout],
+  "s3_access_key_id", node[:enchant][:s3_access_key_id],
+  "s3_region": node[:enchant][:s3_region]
+  "s3_bucket": node[:enchant][:s3_bucket]
+  "s3_secret_access_key_id", node[:enchant][:s3_secret_access_key_id],
 }
 
 default[:enchant][:start_cmd] = "unicorn -E production -c /opt/unicorn.rb"
+
